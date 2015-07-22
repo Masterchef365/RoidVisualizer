402fd5d795453d2fdbe95bf6eb31102efa37a4b5
Duncan Freeman <duncan.freeman1@gmail.com>
2015-07-21 20:13:41 -0700

    Ooops
    Forgot that...

---
+++ RoidVisualizer/Assets/FilterObjectAction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FilterObjectAction : MonoBehaviour {
+
+	//THIS IS BAD PRACTICE. NOTE TO SELF: FIX THIS SOON, DO NOT DO AGAIN...
+	GameObject cam;
+	void Start () {
+		cam = Camera.main.gameObject;
+	}
+
+	public void action (string arg) {
+		cam.SendMessage ("Filter", arg);
+		if (arg == "+" | arg == "-") {
+			if (arg == "+") {
+				GameObject madeNew = GameObject.Instantiate(gameObject);
+				madeNew.transform.parent = transform.parent;
+			} else {
+				if (gameObject.name.Contains("(Clone)")) {
+					Destroy(gameObject);
+				}
+			}
+		} 
+	}
+}
